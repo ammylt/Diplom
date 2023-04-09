f5679ffbe918073e499a4d8424bd2cb86ae25de0
ammylt <80398061+ammylt@users.noreply.github.com>
2023-04-09 16:05:56 +0300

    Add

---
+++ src/components/Test1Task3/Test1Task3.tsx
@@ -0,0 +1,141 @@
+import React from 'react';
+import {BlueColumn} from "../../components/blueColumn/blueColumn";
+import CegmenSmesh from "../../assets/cegment-smesh.jpg"
+import Efremov from "../../assets/efremov.jpg";
+export const Test1Task3 = (props:any) => {
+    return(
+        <div>
+            <span className="h_1">3 Задание.</span>
+            <br />
+            Составьте таблицу истинности для одной из функций a,b,c…m, управляющих работой соответствующего сегмента индикатора десятичной цифры, представленной в коде Д1 для нечётных вариантов,
+            и в коде Д2 для чётных вариантов.
+            <br />
+            Смещение: {props.smesh}, сегмент: {props.cegment}
+            <br />
+            <br />
+            <img className="image_1" src={CegmenSmesh} alt="CegmentSmesh"/>
+            <br />
+            <table>
+                <tr>
+                    <th>x1</th>
+                    <th>x2</th>
+                    <th>x3</th>
+                    <th>x4</th>
+                    <th>{props.cegment}</th>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+                <tr>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                    <td><input className="i_2"/></td>
+                </tr>
+            </table>
+        </div>
+    )
+}
---
+++ src/components/Test1Task7/Test1Task7.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const Test1Task7 = () => {
+    return (
+        <div>
+            <br />
+            7 Задание.
+            <br />
+            Выполните синтез функции в базисах: основном, Шеффера, Пирса.
+            <br />
+            Решение = <textarea className="i_4"/>
+        </div>
+    )
+}
